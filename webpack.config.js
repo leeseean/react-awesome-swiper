1854033e7dbc64162fbf43e32dff8e680372471b
leeseean chiu <leeseean@qq.com>
2018-10-02 02:26:43 +0800

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+    entry: {
+        'reactSwiper': './index.jsx',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: 'reactSwiper'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx$/,
+            loader: 'babel-loader',
+        }, {
+            test: /\.css$/,
+            loader: 'css-loader',
+        }]
+    },
+    //压缩js
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    minimize: false,
+                    compress: false,
+                }
+            })
+        ]
+    },
+};
