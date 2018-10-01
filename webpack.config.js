1c3b93f9b11ce2d7b4eb2fabbf60459e39e3e2f6
leeseean chiu <leeseean@qq.com>
2018-10-02 02:37:09 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        library: 'reactSwiper'
+        library: 'reactSwiper',
+        libraryTarget: 'umd'
     },
     module: {
         rules: [{
