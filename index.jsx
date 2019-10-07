ecfd771e8ed813a41745efa96695bea07c0485bd
leeseean chiu <leeseean@qq.com>
2019-10-07 22:19:51 +0800

    improve

--- index.jsx
+++ index.jsx
@@ -3,13 +3,16 @@ import Swiper from 'swiper';
 import 'swiper/dist/css/swiper.min.css';
 
 class ReactAwesomeSwiper extends React.Component {
-    swiper = null
+    constructor() {
+        this.id = `swiper${new Date().getTime()}`;
+        this.swiper = null;
+    }
     componentDidMount() {
-        this.swiper = new Swiper('.swiper-container', this.props.config);
+        this.swiper = new Swiper(`#${this.id}`, this.props.config);
     }
     componentDidUpdate() {
         this.swiper.destroy();
-        this.swiper = new Swiper('.swiper-container', this.props.config);
+        this.swiper = new Swiper(`#${this.id}`, this.props.config);
     }
     componentWillUnmount() {
         this.swiper.destroy();
@@ -17,7 +20,7 @@ class ReactAwesomeSwiper extends React.Component {
     render() {
         const { config, className, ...rest } = this.props;
         return (
-            <div className={`swiper-container ${className}`} {...rest}>
+            <div id={this.id} className={`swiper-container ${className}`} {...rest}>
                 {this.props.children}
             </div>
         );
